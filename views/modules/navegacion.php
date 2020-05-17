47f8fe2bf996000025d624a9b5e6487f1850661a
anaherreramiranda <anac3714@hotmail.com>
2020-05-17 17:37:56 -0500

    Update navegacion.php

--- views/modules/navegacion.php
+++ views/modules/navegacion.php
@@ -1,8 +1,8 @@
 <nav>
     <ul>
-        <li><a href="">Inicio</a></li>
-        <li><a href="">Nosotros</a></li>
-        <li><a href="">Servicios</a></li>
-        <li><a href="">Contáctenos</a></li>
+        <li><a href="index.php">Inicio</a></li>
+        <li><a href="index.php?action=nosotros">Nosotros</a></li>
+        <li><a href="index.php?action=servicios">Servicios</a></li>
+        <li><a href="index.php?action=contactenos">Contáctenos</a></li>
     </ul>
 </nav>
