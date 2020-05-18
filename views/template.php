d97bf9ff4b834ff5086dfb654e7cb93567818c45
anaherreramiranda <anac3714@hotmail.com>
2020-05-17 21:10:08 -0500

    se realiza nuevamente la codificacion

---
+++ views/template.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Template</title>
+
+    <style>
+
+        header{
+            position: relative;
+            margin: auto;
+            text-align: center;
+            padding: 5px;
+        }
+
+        nav{
+            position: relative;
+            margin: auto;
+            width: 100%;
+            height: auto;
+            background: black;
+        }
+
+        nav ul{
+            position: relative;
+            margin: auto;
+            width: 50%;
+            text-align: center;
+        }
+
+        nav ul li{
+            display: inline-block;
+            width: 24%;
+            line-height: 50px;
+            list-style: none;
+        }
+
+        nav ul li a{
+            color: white;
+            text-decoration: none;
+        }
+
+        section{
+            position: relative;
+            padding: 20px
+        }
+
+    </style>
+</head>
+<body>
+
+<header>
+        <h1>LOGOTIPO</h1>
+</header>
+
+<nav>
+    <ul>
+        <li><a href="">Inicio</a></li>
+        <li><a href="">Nosotros</a></li>
+        <li><a href="">Servicios</a></li>
+        <li><a href="">Contáctenos</a></li>
+    </ul>
+</nav>
+
+<section>
+    <h1>PÁGINA DE INICIO</h1>
+</section>
+    
+</body>
+</html>
