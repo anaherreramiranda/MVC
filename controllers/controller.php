f610a947910628593931cc7df627f9439e9bf102
anaherreramiranda <anac3714@hotmail.com>
2020-05-17 21:40:19 -0500

    nueva codificacion

--- controllers/controller.php
+++ controllers/controller.php
@@ -2,9 +2,22 @@
 
 class MvcController{
 
+    #LLAMADA A LA PLANTILLA
+    #----------------------------------------
+    
     public function plantilla(){
         
         include "views/template.php";
+    }
+
+    #LLAMADA A LA PLANTILLA
+    #----------------------------------------
+    public function enlacesPaginasController(){
+
+        $enlaces = $_GET["action"];
+
+        echo $enlaces;
+        
     }
 }
 
--- views/template.php
+++ views/template.php
@@ -54,17 +54,18 @@
         <h1>LOGOTIPO</h1>
 </header>
 
-<nav>
-    <ul>
-        <li><a href="">Inicio</a></li>
-        <li><a href="">Nosotros</a></li>
-        <li><a href="">Servicios</a></li>
-        <li><a href="">Contáctenos</a></li>
-    </ul>
-</nav>
+<?php
+    include "modules/navegacion.php";
+?>
 
 <section>
-    <h1>PÁGINA DE INICIO</h1>
+
+<?php
+
+$mvc = new MvcController();
+$mvc -> enlacesPaginasController();
+
+?>
 </section>
     
 </body>
